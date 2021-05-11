3ac44319e966a1d7cfe4e2b1e755443f21ad6a5a
Gabriel Bolbotina <bolbotinagabriel@gmail.com>
2021-05-11 22:43:44 +0300

    Added login feature

---
+++ src/main/java/org/FIS2021/controllers/LoginController.java
@@ -0,0 +1,73 @@
+package org.FIS2021.controllers;
+
+
+
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.text.Text;
+import javafx.stage.Stage;
+import org.FIS2021.exceptions.UserNotFoundException;
+import org.FIS2021.services.UserService;
+
+import java.io.IOException;
+
+public class LoginController {
+
+    @FXML
+    public Text loginMessage;
+    @FXML
+    public PasswordField passwordField;
+    @FXML
+    public TextField usernameField;
+
+    @FXML
+    public void handleLoginAction() {
+        String username = usernameField.getText();
+        String password = passwordField.getText();
+
+        if (username == null || username.isEmpty()) {
+            loginMessage.setText("Please type in a username!");
+            return;
+        }
+
+        if (password == null || password.isEmpty()) {
+            loginMessage.setText("Password cannot be empty");
+            return;
+        }
+
+        String encoded_password = UserService.encodePassword(username, password);
+
+        try{
+            String stored_password = UserService.getHashedUserPassword(username);
+            if(stored_password.equals(encoded_password)){
+               loginMessage.setText(String.format("Logged in as %s!", username));
+                return;
+            }
+
+        } catch(UserNotFoundException e){
+            loginMessage.setText(e.getMessage());
+            return;
+        }
+
+        loginMessage.setText("Incorrect login!");
+    }
+
+    @FXML
+    public void loadRegisterPage(){
+        try {
+            Stage stage = (Stage) loginMessage.getScene().getWindow();
+            Parent registerRoot = FXMLLoader.load(getClass().getResource("/FXML/Register.fxml"));
+            Scene scene = new Scene(registerRoot, 640, 800);
+            stage.setTitle("Forum App - Register");
+            stage.setScene(scene);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+}
--- src/main/java/org/FIS2021/controllers/RegistrationController.java
+++ src/main/java/org/FIS2021/controllers/RegistrationController.java
@@ -2,11 +2,16 @@ package org.FIS2021.controllers;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.ChoiceBox;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.scene.text.Text;
-import org.FIS2021.exceptions.UsernameAlreadyExistsException;
+import javafx.stage.Stage;
 import org.FIS2021.services.UserService;
+import org.FIS2021.exceptions.UsernameAlreadyExistsException;
+import java.io.IOException;
 public class RegistrationController {
 
     @FXML
@@ -30,6 +35,18 @@ public class RegistrationController {
             registrationMessage.setText("Account created successfully!");
         } catch (UsernameAlreadyExistsException e) {
             registrationMessage.setText(e.getMessage());
+        }
+    }
+    @FXML
+    public void loadLoginPage(){
+        try {
+            Stage stage = (Stage) registrationMessage.getScene().getWindow();
+            Parent viewStudentsRoot = FXMLLoader.load(getClass().getResource("/FXML/Login.fxml"));
+            Scene scene = new Scene(viewStudentsRoot, 640, 480);
+            stage.setTitle("Plant Store - Log in");
+            stage.setScene(scene);
+        } catch (IOException e) {
+            e.printStackTrace();
         }
     }
 }
