3919278c76ccd852567528f905ba97eb9b378623
Gabriel Bolbotina <bolbotinagabriel@gmail.com>
2021-05-11 19:21:08 +0300

    PS-24 added registration

--- src/main/java/module-info.java
+++
@@ -1,7 +0,0 @@
-module org.FIS2021 {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-    opens org.FIS2021 to javafx.fxml;
-    exports org.FIS2021;
-}
---
+++ src/main/java/org/FIS2021/controllers/RegistrationController.java
@@ -0,0 +1,35 @@
+package org.FIS2021.controllers;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.text.Text;
+import org.FIS2021.exceptions.UsernameAlreadyExistsException;
+import org.FIS2021.services.UserService;
+public class RegistrationController {
+
+    @FXML
+    private Text registrationMessage;
+    @FXML
+    private PasswordField passwordField;
+    @FXML
+    private TextField usernameField;
+    @FXML
+    private ChoiceBox role;
+
+    @FXML
+    public void initialize() {
+        role.getItems().addAll("Provider", "Client", "Manager");
+    }
+
+    @FXML
+    public void handleRegisterAction() {
+        try {
+            UserService.addUser(usernameField.getText(), passwordField.getText(), (String) role.getValue());
+            registrationMessage.setText("Account created successfully!");
+        } catch (UsernameAlreadyExistsException e) {
+            registrationMessage.setText(e.getMessage());
+        }
+    }
+}
