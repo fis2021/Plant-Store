ba24bc213231daa194b294a21f2e93406ee8575a
Gabriel Bolbotina <bolbotinagabriel@gmail.com>
2021-05-13 18:58:30 +0300

    PS-11b differential homepage according to the role and homepage log out fucntion

---
+++ src/main/java/org/FIS2021/controllers/HomepageClientController.java
@@ -0,0 +1,58 @@
+package org.FIS2021.controllers;
+
+import org.FIS2021.models.User;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.text.Text;
+import javafx.stage.Stage;
+
+import javafx.scene.control.*;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+
+import java.io.IOException;
+
+public class HomepageClientController {
+    @FXML
+    private Button butonCreareCont;
+    @FXML
+    private Button butonAlegereAngajat;
+    @FXML
+    private Button butonLogOut;
+    @FXML
+    private Button butonVizualizareListaServiciiSiPreturi;
+    @FXML
+    private Button AlegereOptiuneDorita;
+    @FXML
+    private Button buttonCreate;
+    @FXML
+    private Text registrationMessage;
+
+    @FXML
+    private User user;
+
+    public void setUser(User user)
+    {
+        this.user=user;
+    }
+
+
+
+
+    @FXML
+    private void handleLoginAction(){
+        try {
+            Stage stage = (Stage) butonLogOut.getScene().getWindow();
+            Parent loginRoot = FXMLLoader.load(getClass().getResource("/FXML/Login.fxml"));
+            Scene scene = new Scene(loginRoot, 640, 480);
+            stage.setTitle("Plant Store -login");
+            stage.setScene(scene);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
